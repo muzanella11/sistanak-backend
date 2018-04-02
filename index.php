7c61b2693ecb7cdf765aa809b48000607b64f669
Nurfirliana Muzanella <muzanella11@gmail.com>
2018-04-03 01:37:35 +0700

    [FIX] CORS

--- index.php
+++ index.php
@@ -312,4 +312,8 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 require_once BASEPATH.'core/CodeIgniter.php';
