8014ce961ae9b62bcf6ac18c2031d0405d81f930
Nurfirliana Muzanella <muzanella11@gmail.com>
2018-04-07 20:38:22 +0700

    [FIX] Environment model

--- application/controllers/environment/Data.php
+++ application/controllers/environment/Data.php
@@ -51,11 +51,123 @@ class Data extends RestManager {
             }
 
             $dataModel[0]['filter'] = 'create_sql';
-            $dataModel[0]['filterKey'] = 'address like "%'.$queryString['q'].'%" or environment_id like "%'.$queryString['environment'].'%" or province_id like "%'.$queryString['province'].'%" or district_id like "%'.$queryString['district'].'%" or village_id like "%'.$queryString['village'].'%"';
+            $dataModel[0]['filterKey'] = $queryString['q'] !== 'null' ? 'WHERE address like "%'.$queryString['q'].'%" or land_area like "%'.$queryString['q'].'%"' : null;
             $dataModel[0]['fieldTarget'] = null;
         }
         
         $data = $this->CrudManagement->run($config, $dataModel);
+
+        $getTotalData = $this->CrudManagement->run($config, $dataModel);
+
+        $data['totalData'] = count($getTotalData['data']);
+
+        foreach ($data['data'] as $key => $value) {
+            $dataMaster = json_encode($data['data'][$key]);
+            $dataMasterEncode = json_decode($dataMaster, TRUE);
+            $data['data'][$key] = $dataMasterEncode;
+            $provinceId = (int) $data['data'][$key]['province_id'];
+            $districtId = (int) $data['data'][$key]['district_id'];
+            $villageId = (int) $data['data'][$key]['village_id'];
+            $drainase = (int) $data['data'][$key]['drainase'];
+            $environmentId = (int) $data['data'][$key]['environment_id'];
+            $foodAvail = (int) $data['data'][$key]['food_availability'];
+            $fount = (int) $data['data'][$key]['fount'];
+            $hygiene = (int) $data['data'][$key]['hygiene'];
+            $pollution = (int) $data['data'][$key]['pollution'];
+            $landArea = (int) $data['data'][$key]['land_area'];
+
+            $data['data'][$key]['province_id'] = $provinceId;
+            $data['data'][$key]['district_id'] = $districtId;
+            $data['data'][$key]['village_id'] = $villageId;
+            $data['data'][$key]['drainase'] = $drainase;
+            $data['data'][$key]['environment_id'] = $environmentId;
+            $data['data'][$key]['food_availability'] = $foodAvail;
+            $data['data'][$key]['fount'] = $fount;
+            $data['data'][$key]['hygiene'] = $hygiene;
+            $data['data'][$key]['pollution'] = $pollution;
+            $data['data'][$key]['land_area'] = $landArea;
+        }
+
+        // Get data province
+        $dataModelProvinceDetail = [
+            [
+                'className' => 'Provinces',
+                'modelName' => 'ProvincesModel',
+                'filter' => 'id',
+                'filterKey' => '',
+                'limit' => null,
+                'fieldTarget' => 'name',
+                'dataMaster' => []
+            ]
+        ];
+
+        foreach ($data['data'] as $key => $value) {
+            if ($value['province_id'])
+            {
+                $dataModelProvinceDetail[0]['filterKey'] = $value['province_id'];
+                $dataProvinceDetail = $this->CrudManagement->run($config, $dataModelProvinceDetail);
+                // var_dump($dataProvinceDetail);exit;
+                $dataMaster = json_encode($data['data'][$key]);
+                $dataMasterEncode = json_decode($dataMaster, TRUE);
+                $dataMasterEncode['province_detail'] = count($dataProvinceDetail['data']) > 0 ? $dataProvinceDetail['data'][0] : [];
+                $dataMasterResult = $dataMasterEncode;
+                $data['data'][$key] = $dataMasterResult;
+            }
+        }
+
+        // Get data region
+        $dataModelRegionDetail = [
+            [
+                'className' => 'Regencies',
+                'modelName' => 'RegenciesModel',
+                'filter' => 'id',
+                'filterKey' => '',
+                'limit' => null,
+                'fieldTarget' => 'name',
+                'dataMaster' => []
+            ]
+        ];
+
+        foreach ($data['data'] as $key => $value) {
+            if ($value['district_id'])
+            {
+                $dataModelRegionDetail[0]['filterKey'] = $value['district_id'];
+                $dataRegionDetail = $this->CrudManagement->run($config, $dataModelRegionDetail);
+
+                $dataMaster = json_encode($data['data'][$key]);
+                $dataMasterEncode = json_decode($dataMaster, TRUE);
+                $dataMasterEncode['region_detail'] = count($dataRegionDetail['data']) > 0 ? $dataRegionDetail['data'][0] : [];
+                $dataMasterResult = $dataMasterEncode;
+                $data['data'][$key] = $dataMasterResult;
+            }
+        }
+
+        // Get data village
+        $dataModelVillageDetail = [
+            [
+                'className' => 'Villages',
+                'modelName' => 'VillagesModel',
+                'filter' => 'id',
+                'filterKey' => '',
+                'limit' => null,
+                'fieldTarget' => 'name',
+                'dataMaster' => []
+            ]
+        ];
+
+        foreach ($data['data'] as $key => $value) {
+            if ($value['village_id'])
+            {
+                $dataModelVillageDetail[0]['filterKey'] = $value['village_id'];
+                $dataVillageDetail = $this->CrudManagement->run($config, $dataModelVillageDetail);
+                
+                $dataMaster = json_encode($data['data'][$key]);
+                $dataMasterEncode = json_decode($dataMaster, TRUE);
+                $dataMasterEncode['village_detail'] = count($dataVillageDetail['data']) > 0 ? $dataVillageDetail['data'][0] : [];
+                $dataMasterResult = $dataMasterEncode;
+                $data['data'][$key] = $dataMasterResult;
+            }
+        }
 
         if ($data['status'] === 'Problem')
         {
